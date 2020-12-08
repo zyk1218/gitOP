ba4daa1ec473fdba057f8f8e57b1df80c9e4dc17
remember <1977293486@qq.com>
2020-12-08 17:51:22 +0800

    添加Develop文件（该文件原本存在，使用git rm后在远程仓库同样销售了）

---
+++ src/main/java/com/example/practice/git/git/Develop.java
@@ -0,0 +1,8 @@
+package com.example.practice.git.git;
+
+public class Develop {
+    public void develop(){
+        System.out.println("开发分支");
+        System.out.println("test delete added files");
+    }
+}
